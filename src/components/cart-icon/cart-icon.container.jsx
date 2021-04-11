34f10444346ddfcdd4cfb9833bbd4c18ea809e7a
Alan <alancurtis5000+accounts@gmail.com>
2021-04-11 14:14:53 -0600

    259: cart item count using flowRight /compose

--- src/components/cart-icon/cart-icon.container.jsx
+++ src/components/cart-icon/cart-icon.container.jsx
@@ -1,4 +1,5 @@
-import { Mutation, Query } from "react-apollo";
+import { graphql } from "react-apollo";
+import flowRight from "lodash/flowRight";
 import { gql } from "apollo-boost";
 
 import CartIcon from "./cart-icon.component";
@@ -15,7 +16,24 @@ const GET_ITEM_COUNT = gql`
     itemCount @client
   }
 `;
+// version 2 using flowRight and higher order component structure similare to redux connect
+const CartIconContainer = (props) => {
+  const { itemCount, toggleCartHidden } = props;
+  return <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />;
+};
 
+export default flowRight(
+  graphql(GET_ITEM_COUNT),
+  // second prop being passed in is a config object where we can specify the name of whats returns as props name.
+  graphql(TOGGLE_CART_HIDDEN, { name: "toggleCartHidden" })
+)(CartIconContainer);
+
+///////////////////////////
+///////////////////////////
+///////////////////////////
+
+/* 
+// version using Query and Mutation Component functions
 const CartIconContainer = () => (
   <Query query={GET_ITEM_COUNT}>
     {({ data: { itemCount } }) => (
@@ -29,3 +47,4 @@ const CartIconContainer = () => (
 );
 
 export default CartIconContainer;
+*/
